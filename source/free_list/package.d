59c1a31614d9e39cc932748e56031cce15c3fbee
malyjacob <malyjacob926@outlook.com>
2024-11-17 15:18:39 +0800

    add shared free-list, and change the goodAllocSize to const

--- source/free_list/package.d
+++ source/free_list/package.d
@@ -172,7 +172,7 @@ struct FreeList(ParentAllocator,
 
 	// get the good size.
 	@nogc nothrow @safe pure
-	size_t goodAllocSize(size_t bytes) {
+	size_t goodAllocSize(size_t bytes) const {
 		assert(bytes);
 		assert(!(min == chooseAtRuntime || max == chooseAtRuntime));
 		if (!freeListEligible(bytes))
@@ -741,3 +741,641 @@ struct FreeList(ParentAllocator,
 
 // 	test();
 // }
+
+shared struct SharedFreeList(ParentAllocator,
+	size_t minSize, size_t maxSize = minSize,
+	Flag!"adaptive" adaptive = No.adaptive, uint theAlignment = platformAlignment) {
+
+	import std.traits : hasMember;
+	import std.typecons : Ternary;
+	import std.math.traits;
+	import std.algorithm.comparison : clamp;
+	import std.algorithm.mutation : fill;
+	static import std.algorithm.comparison;
+	import core.stdc.string : memmove;
+	import core.atomic : atomicOp, cas, atomicStore;
+	import core.internal.spinlock : SpinLock;
+	import std.experimental.allocator;
+
+	import std.experimental.allocator.mallocator;
+	import std.exception : enforce;
+
+	private shared struct Node {
+		Node* next;
+		size_t volum;
+	}
+
+	static assert(ParentAllocator.alignment >= theAlignment);
+	static assert(theAlignment >= Node.alignof);
+
+	alias alignment = theAlignment;
+
+	static assert(maxSize >= alignment, "Maximum size must be equal or greater than its alignment.");
+	static assert(minSize != unbounded, "Use minSize = 0 for no low bound.");
+
+	static if (stateSize!ParentAllocator)
+		ParentAllocator parent;
+	else
+		alias parent = ParentAllocator.instance;
+
+	static if (minSize == chooseAtRuntime)
+		private size_t _min = chooseAtRuntime;
+	static if (maxSize == chooseAtRuntime)
+		private size_t _max = chooseAtRuntime;
+
+	private Node* allocatedRoot;
+	private Node* freeRoot;
+
+	private SpinLock allocatedLock;
+	private SpinLock freeLock;
+
+	static if (minSize == chooseAtRuntime) {
+		@nogc nothrow @safe pure
+		@property size_t min() const {
+			assert(_min != chooseAtRuntime);
+			return _min;
+		}
+
+		@nogc nothrow
+		@property void min(size_t low) {
+			assert(allocatedRoot is null);
+			assert(low <= _max || _max == chooseAtRuntime);
+			minimize;
+			_min = low;
+		}
+	}
+	else {
+		alias min = minSize;
+	}
+
+	static if (maxSize == chooseAtRuntime) {
+		@nogc nothrow @safe pure
+		@property size_t max() const {
+			assert(_max != chooseAtRuntime);
+			return _max;
+		}
+
+		@nogc nothrow
+		@property void max(size_t high) {
+			assert(allocatedRoot is null);
+			assert((high >= _min || _min == chooseAtRuntime)
+					&& high >= alignment);
+			minimize;
+			_max = high;
+		}
+	}
+	else {
+		alias max = maxSize;
+	}
+
+	static if (minSize == chooseAtRuntime && maxSize == chooseAtRuntime) {
+		void setBounds(size_t low, size_t high) {
+			enforce(low <= high && high >= (void*).sizeof);
+			enforce(cas(&_min, chooseAtRuntime, low),
+				"SharedFreeList.min must be initialized exactly once.");
+			enforce(cas(&_max, chooseAtRuntime, high),
+				"SharedFreeList.max must be initialized exactly once.");
+		}
+	}
+
+	@nogc nothrow @safe pure
+	private bool tooSmall(size_t n) const {
+		static if (minSize == 0)
+			return false;
+		else
+			return n < min;
+	}
+
+	@nogc nothrow @safe pure
+	private bool tooLarge(size_t n) const {
+		static if (maxSize == unbounded)
+			return false;
+		else
+			return n > max;
+	}
+
+	@nogc nothrow @safe pure
+	private bool freeListEligible(size_t n) const {
+		if (min == 0 && max == unbounded)
+			return true;
+		if (min == 0 && !n)
+			return false;
+		if (min == max)
+			return n == max;
+		else
+			return !tooSmall(n) && !tooLarge(n);
+	}
+
+	@nogc nothrow @safe pure
+	size_t goodAllocSize(size_t bytes) const {
+		assert(bytes);
+		assert(!(min == chooseAtRuntime || max == chooseAtRuntime));
+		if (!freeListEligible(bytes))
+			return unbounded;
+		if (max != unbounded)
+			return max.roundUpToAlignment(alignment);
+		return bytes.roundUpToAlignment(alignment);
+	}
+
+	static if (adaptive == Yes.adaptive) {
+		private int recentDeallocateNum = 0;
+
+		nothrow private void updateStats(size_t requiredAllocates, uint a) {
+			auto aligned_node_size = Node.sizeof.roundUpToAlignment(a);
+			immutable size_t extra_block_num = requiredAllocates < (1024 - aligned_node_size) ?
+				2 : requiredAllocates < (4096 - aligned_node_size) ? 1 : 0;
+			void[] blk;
+			foreach (_; 0 .. extra_block_num) {
+				if (a != alignment) {
+					static if (hasMember!(ParentAllocator, "alignedAllocate"))
+						blk = parent.alignedAllocate(aligned_node_size + requiredAllocates, a);
+				}
+				else
+					blk = parent.allocate(requiredAllocates + aligned_node_size);
+				if (blk is null)
+					return;
+				Node* node = cast(Node*) blk.ptr;
+				node.volum = blk.length - Node.sizeof;
+				node.next = freeRoot;
+				freeLock.lock();
+				freeRoot = node;
+				freeLock.unlock();
+			}
+		}
+
+		static if (hasMember!(ParentAllocator, "deallocate"))
+			nothrow private void updateStats(size_t threshold) {
+				auto num = clamp(recentDeallocateNum - 1, 0, 8);
+				freeLock.lock();
+				Node* cur = freeRoot, prev;
+				while (cur && cur.volum >= threshold) {
+					prev = cur;
+					cur = cur.next;
+				}
+				while (cur && num) {
+					auto target = blockFor(cur);
+					cur = cur.next;
+					parent.deallocate(target);
+				}
+				prev ? (prev.next = cur) : (freeRoot = cur);
+				freeLock.unlock();
+				atomicStore(recentDeallocateNum, 0);
+			}
+		else
+			@nogc nothrow @safe pure
+			private void updateStats(size_t threshold) {
+			}
+	}
+
+	static if (maxSize != chooseAtRuntime)
+		this(size_t expectedInitialNum, size_t bytes = maxSize) nothrow {
+			assert(expectedInitialNum != 0 && bytes > 0 && bytes != unbounded && bytes >= minSize);
+			static if (maxSize != unbounded)
+				assert(bytes == maxSize);
+			auto aligned_node_size = Node.sizeof.roundUpToAlignment(alignment);
+			auto required_allocates = goodAllocSize(bytes);
+			void[] blk;
+			foreach (_; 0 .. expectedInitialNum) {
+				blk = parent.allocate(required_allocates + aligned_node_size);
+				if (blk is null)
+					return;
+				Node* node = cast(Node*) blk.ptr;
+				node.volum = blk.length - Node.sizeof;
+				node.next = freeRoot;
+				freeRoot = node;
+			}
+		}
+
+	@nogc nothrow @trusted pure
+	private void[] blockFor(const Node* p) const {
+		assert(p);
+		return (cast(void*) p)[0 .. p.volum + Node.sizeof];
+	}
+
+	@nogc nothrow @trusted
+	private Node* nodeFor(Flag!"withlock" withlock = Yes.withlock)(const void* p) {
+		if (!p)
+			return null;
+		static if (withlock == Yes.withlock)
+			allocatedLock.lock();
+		shared cur = cast() allocatedRoot;
+		while (cur) {
+			auto pos = cast(size_t) p;
+			auto bottom = (cast(size_t) cur + Node.sizeof).roundUpToAlignment(alignment);
+			auto top = (cast(size_t) cur + Node.sizeof + cur.volum);
+			if (pos >= bottom && pos < top) {
+				allocatedLock.unlock();
+				return cur;
+			}
+			cur = cur.next;
+		}
+		static if (withlock == Yes.withlock)
+			allocatedLock.unlock();
+		return null;
+	}
+
+	@nogc nothrow @safe
+	Ternary owns(const void[] b) {
+		return nodeFor(&b[0]) is null ? Ternary.no : Ternary.yes;
+	}
+
+	@nogc nothrow Ternary resolveInternalPointer(const void* p, ref void[] result) {
+		return resolveInternalPointer(p, alignment, result);
+	}
+
+	@nogc nothrow Ternary resolveInternalPointer(const void* p, uint a, ref void[] result) {
+		assert(a >= alignment);
+		allocatedLock.lock();
+		auto node = nodeFor!(No.withlock)(p);
+		if (!node) {
+			allocatedLock.unlock();
+			return Ternary.no;
+		}
+		result = blockFor(node)[Node.sizeof .. $].roundUpToAlignment(a);
+		allocatedLock.unlock();
+		return Ternary.yes;
+	}
+
+	nothrow private void[] allocateEligible(string fillMode)(size_t bytes, uint a = cast(uint) alignment)
+		if (fillMode == "void" || fillMode == "zero") {
+		assert(a >= alignment && a.isPowerOf2);
+		enum bool is_fill_zero = fillMode == "zero";
+		static if (adaptive == Yes.adaptive)
+			size_t threshold;
+		auto required_allocates = goodAllocSize(bytes);
+		freeLock.lock();
+		Node* cur = freeRoot, prev = null;
+		while (cur) {
+			auto blk = blockFor(cur)[Node.sizeof .. $].roundUpToAlignment(a);
+			if (blk.length < required_allocates) {
+				prev = cur;
+				cur = cur.next;
+				continue;
+			}
+			static if (adaptive == Yes.adaptive) {
+				if (cur.volum >= 2 * required_allocates) {
+					threshold = cur.volum;
+					break;
+				}
+			}
+			prev ? (prev.next = cur.next) : (freeRoot = cur.next);
+			freeLock.unlock();
+			cur.next = allocatedRoot;
+			allocatedLock.lock();
+			allocatedRoot = cur;
+			allocatedLock.unlock();
+			return blk[0 .. bytes];
+		}
+		freeLock.unlock();
+		void[] blk;
+		auto aligned_node_size = Node.sizeof.roundUpToAlignment(a);
+		if (a != alignment) {
+			static if (hasMember!(ParentAllocator, "alignedAllocate"))
+				blk = parent.alignedAllocate(aligned_node_size + required_allocates, a);
+			else
+				return null;
+		}
+		else
+			blk = parent.allocate(required_allocates + aligned_node_size);
+		if (blk is null)
+			return null;
+		static if (is_fill_zero) {
+			auto tmp = cast(ubyte[]) blk;
+			tmp.fill(cast(ubyte) 0);
+		}
+		Node* node = cast(Node*) blk.ptr;
+		node.volum = blk.length - Node.sizeof;
+		node.next = allocatedRoot;
+		allocatedLock.lock();
+		allocatedRoot = node;
+		allocatedLock.unlock();
+		static if (adaptive == Yes.adaptive) {
+			if (threshold > 0 || (max != unbounded && !cur))
+				updateStats(threshold);
+			updateStats(required_allocates, a);
+		}
+		return blk[aligned_node_size .. aligned_node_size + bytes];
+	}
+
+	nothrow void[] allocate(size_t n) {
+		if (freeListEligible(n))
+			return allocateEligible!"void"(n);
+		return null;
+	}
+
+	nothrow void[] allocateZeroed(size_t n) {
+		if (freeListEligible(n))
+			return allocateEligible!"zero"(n);
+		return null;
+	}
+
+	static if (hasMember!(ParentAllocator, "alignedAllocate"))
+		nothrow void[] alignedAllocate(size_t n, uint a) {
+			assert(a >= alignment && isPowerOf2(a));
+			if (freeListEligible(n))
+				return allocateEligible!"void"(n, a);
+			return null;
+		}
+
+	nothrow bool reallocate(ref void[] b, size_t s) {
+		if (!b.ptr) {
+			b = allocate(s);
+			return b.length == s;
+		}
+		if (s == 0) {
+			if (!deallocate(b))
+				return false;
+			b = null;
+			return true;
+		}
+		if (b.length == s)
+			return true;
+		else if (b.length < s) {
+			if (alignedMove(b, s, alignment))
+				return true;
+			auto new_blk = allocate(s);
+			if (new_blk.length != s)
+				return false;
+			new_blk[0 .. b.length] = b[];
+			if (!deallocate(b))
+				return false;
+			b = new_blk;
+			return true;
+		}
+		else {
+			b = b[0 .. s];
+			return true;
+		}
+	}
+
+	static if (hasMember!(ParentAllocator, "alignedAllocate"))
+		nothrow bool alignedReallocate(ref void[] b, size_t s, uint a) {
+			assert(a >= alignment && isPowerOf2(a));
+			if (a == alignment)
+				return reallocate(b, s);
+			if (!b.ptr) {
+				b = alignedAllocate(s, a);
+				return b.length == s;
+			}
+			if (s == 0) {
+				if (!deallocate(b))
+					return false;
+				b = null;
+				return true;
+			}
+			if (cast(size_t) b.ptr % a != 0)
+				goto LAlignedMoveOrAllocate;
+			else {
+				if (b.length == s)
+					return true;
+				else if (b.length < s)
+					goto LAlignedMoveOrAllocate;
+				else {
+					b = b[0 .. s];
+					return true;
+				}
+			}
+		LAlignedMoveOrAllocate:
+			auto result = alignedMove(b, s, a);
+			if (result)
+				return true;
+			auto new_blk = alignedAllocate(s, a);
+			if (new_blk.length != s)
+				return false;
+			if (b.length <= s)
+				new_blk[0 .. b.length] = b[];
+			else
+				new_blk[] = b[0 .. s];
+			if (!deallocate(b))
+				return false;
+			b = new_blk;
+			return true;
+		}
+
+	@nogc nothrow bool expand(ref void[] b, size_t delta) {
+		return alignedExpand(b, delta, alignment);
+	}
+
+	@nogc nothrow bool alignedExpand(ref void[] b, size_t delta, uint a) {
+		assert(a >= alignment && isPowerOf2(a));
+		return alignedMove(b, b.length + delta, a);
+	}
+
+	@nogc nothrow private bool alignedMove(ref void[] b, size_t s, uint a) {
+		if (b is null || s == 0)
+			return false;
+		allocatedLock.lock();
+		Node* node = nodeFor!(No.withlock)(b.ptr);
+		if (!node) {
+			allocatedLock.unlock();
+			return false;
+		}
+		void[] blk = blockFor(node);
+		static if (hasMember!(ParentAllocator, "expand"))
+			if ((b.length < s || a != alignment) && parent.expand(blk, s - b.length)) {
+				node.volum = blk.length - Node.sizeof;
+			}
+		blk = roundUpToAlignment(blk[Node.sizeof .. $], a);
+		if (blk is null) {
+			allocatedLock.unlock();
+			return false;
+		}
+		b = memmove(blk.ptr, b.ptr, std.algorithm.comparison.min(b.length, s))[0 .. s];
+		allocatedLock.unlock();
+		return true;
+	}
+
+	@nogc nothrow bool deallocate(void[] b) {
+		if (!b)
+			return true;
+		allocatedLock.lock();
+		Node* cur = allocatedRoot, prev;
+		while (cur) {
+			auto pos = cast(size_t) b.ptr;
+			auto bottom = (cast(size_t) cur + Node.sizeof).roundUpToAlignment(alignment);
+			auto top = (cast(size_t) cur + Node.sizeof + cur.volum);
+			if (pos >= bottom && pos < top)
+				break;
+			prev = cur;
+			cur = cur.next;
+		}
+		if (cur) {
+			prev ? (prev.next = cur.next) : (allocatedRoot = cur.next);
+			allocatedLock.unlock();
+		}
+		else {
+			allocatedLock.unlock();
+			return false;
+		}
+		auto target = cur;
+		auto volum = target.volum;
+		freeLock.lock();
+		cur = freeRoot, prev = null;
+		while (cur) {
+			if (volum <= cur.volum)
+				break;
+			prev = cur;
+			cur = cur.next;
+		}
+		target.next = cur;
+		prev ? (prev.next = target) : (freeRoot = target);
+		freeLock.unlock();
+		static if (adaptive == Yes.adaptive)
+			atomicOp!"+="(recentDeallocateNum, cast(int) 1);
+		return true;
+	}
+
+	@nogc nothrow bool deallocateAll() {
+		Node* cur, prev;
+		int num;
+		allocatedLock.lock();
+		while (allocatedRoot) {
+			++num;
+			freeLock.lock();
+			cur = freeRoot, prev = null;
+			while (cur) {
+				if (allocatedRoot.volum <= cur.volum)
+					break;
+				prev = cur;
+				cur = cur.next;
+			}
+			auto target = allocatedRoot;
+			allocatedRoot = allocatedRoot.next;
+			target.next = cur;
+			prev ? (prev.next) = target : (freeRoot = target);
+			freeLock.unlock();
+		}
+		allocatedLock.unlock();
+		static if (adaptive == Yes.adaptive)
+			atomicOp!"+="(recentDeallocateNum, num);
+		return true;
+	}
+
+	static if (hasMember!(ParentAllocator, "deallocate"))
+		nothrow void release() {
+			allocatedLock.lock();
+			while (allocatedRoot) {
+				auto blk = blockFor(allocatedRoot);
+				allocatedRoot = allocatedRoot.next;
+				parent.deallocate(blk);
+			}
+			allocatedLock.unlock();
+		}
+
+	static if (hasMember!(ParentAllocator, "deallocate"))
+		nothrow void minimize() {
+			freeLock.lock();
+			while (freeRoot) {
+				auto blk = blockFor(freeRoot);
+				freeRoot = freeRoot.next;
+				parent.deallocate(blk);
+			}
+			freeLock.unlock();
+		}
+
+	static if (hasMember!(ParentAllocator, "deallocate"))
+		nothrow void shrink(size_t expectedNum) {
+			assert(expectedNum != 0);
+			freeLock.lock();
+			while (freeRoot && expectedNum) {
+				auto target = freeRoot;
+				freeRoot = freeRoot.next;
+				parent.deallocate(blockFor(target));
+				--expectedNum;
+			}
+			freeLock.lock();
+		}
+
+	static if (hasMember!(ParentAllocator, "deallocate"))
+		 ~this() @trusted {
+			release();
+			minimize();
+		}
+}
+
+@safe unittest {
+	import std.experimental.allocator.common : chooseAtRuntime;
+	import std.experimental.allocator.mallocator : Mallocator;
+
+	shared SharedFreeList!(Mallocator, chooseAtRuntime, chooseAtRuntime) a;
+	a.setBounds(64, 128);
+	assert(a.max == 128);
+	assert(a.min == 64);
+}
+
+@system unittest {
+	import core.thread : ThreadGroup;
+	import std.algorithm.comparison : equal;
+	import std.experimental.allocator.mallocator : Mallocator;
+	import std.range : repeat;
+
+	static shared SharedFreeList!(Mallocator, 64, 128) a;
+
+	assert((() nothrow @safe @nogc => a.goodAllocSize(1))() == unbounded);
+
+	auto b = a.allocate(96);
+	// Ensure deallocate inherits from parent
+	() nothrow @nogc { a.deallocate(b); }();
+
+	void fun() {
+		auto b = cast(size_t[]) a.allocate(96);
+		b[] = cast(size_t)&b;
+
+		assert(b.equal(repeat(cast(size_t)&b, b.length)));
+		() nothrow @nogc { a.deallocate(b); }();
+	}
+
+	auto tg = new ThreadGroup;
+	foreach (i; 0 .. 20) {
+		tg.create(&fun);
+	}
+
+	tg.joinAll();
+}
+
+@system unittest {
+	import std.experimental.allocator.mallocator : Mallocator;
+
+	shared SharedFreeList!(Mallocator, chooseAtRuntime, chooseAtRuntime) a;
+	a.setBounds(0, unbounded);
+	scope (exit)
+		assert((() nothrow @nogc => a.deallocateAll())());
+	auto c = a.allocate(64);
+	assert((() nothrow @nogc => a.reallocate(c, 96))());
+	assert(c.length == 96);
+	// Ensure deallocate inherits from parent
+	() nothrow @nogc { a.deallocate(c); }();
+}
+
+unittest {
+	import std.experimental.allocator;
+	import std.experimental.allocator.mallocator;
+	import std.experimental.allocator.common;
+	import std.concurrency;
+	import core.thread;
+
+	alias Alloc = SharedFreeList!(AlignedMallocator, 0, unbounded, Yes.adaptive);
+
+	static void child_test(Alloc* ptr_alloc, size_t s, uint a) {
+		auto b1 = ptr_alloc.makeArray!char(s);
+		b1[] = 'x';
+		assert(ptr_alloc.expandArray(b1, 4));
+		b1[$ - 4 .. $] = 'y';
+		auto raw_b1 = cast(void[]) b1[];
+		assert(ptr_alloc.alignedReallocate(raw_b1, s + 8, a));
+		b1 = cast(char[]) raw_b1[];
+		b1[$ - 4 .. $] = 'z';
+	}
+
+	static void test() {
+		Alloc alloc;
+		spawn(&child_test, &alloc, 4, 32);
+		spawn(&child_test, &alloc, 4, 64);
+		spawn(&child_test, &alloc, 4, 128);
+		spawn(&child_test, &alloc, 4, 256);
+
+		thread_joinAll();
+	}
+
+	test();
+}
